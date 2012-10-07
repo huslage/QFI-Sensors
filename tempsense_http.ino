ab2c2a77b15af4873d380c817f380416b2338522
Aaron Huslage <huslage@gmail.com>
2012-10-07 09:13:30 -0400

    first commit

--- tempsense_http.ino
+++ tempsense_http.ino
@@ -1,19 +1,20 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <SoftwareSerial.h>
-//#include "LowPower.h"
-#include <PString.h>
+#include <avr/sleep.h>
+#include <avr/power.h>
+#include <avr/wdt.h>
 
-#define ONE_WIRE_BUS 8 // Data wire is pin 4 on the Arduino
+volatile int f_wdt=1;
+
+#define ONE_WIRE_BUS 11 // Data wire is pin 4 on the Arduino
 #define TEMPERATURE_PRECISION 9 // 9 bit precision corresponds to 0.5 degrees C resolution
-#define SENSOR_ID "68161" // Our ID for reports
 
-// The period in milliseconds to wait in between reports. 1 hour = 1000 * 60 * 60 * 1
-//#define REPORT_INTERVAL 3600000
-#define REPORT_INTERVAL 60000
+// The period in milliseconds to wait in between reports. 1 hour = 1000 * 60 * 60 * 1 / 8
+#define REPORT_INTERVAL 450000
+//#define REPORT_INTERVAL 15000
 
-#define COSM_API_KEY "aBUpZ_sy_SeTdiYGic-7m4fkGMOSAKxjd00xR1BFc3lkST0g"
-#define APN "internet.cxn"
+String APN="internet.cxn";
 
 float waterTemp;
 float ambientTemp;
@@ -22,9 +23,21 @@ float ambientTemp;
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
-// These device addresses were determined in advance by running some of the DallasTemperature library example code
-DeviceAddress waterSensor =   {0x28, 0x5E, 0x7A, 0x14, 0x04, 0x00, 0x00, 0x5B};
-DeviceAddress ambientSensor = {0x28, 0x6F, 0x35, 0x14, 0x04, 0x00, 0x00, 0x07};
+// QFI-1
+String SENSOR_ID="68161"; // Our ID for reports
+String COSM_API_KEY="aBUpZ_sy_SeTdiYGic-7m4fkGMOSAKxjd00xR1BFc3lkST0g";
+DeviceAddress waterSensor =   {
+  0x28, 0x5E, 0x7A, 0x14, 0x04, 0x00, 0x00, 0x5B};
+DeviceAddress ambientSensor = {
+  0x28, 0x6F, 0x35, 0x14, 0x04, 0x00, 0x00, 0x07};
+
+/*
+  // QFI-2
+ String SENSOR_ID="69837"; // Our ID for reports
+ String COSM_API_KEY="KoBLICFuh3xUPCe50KU-ltkkV_CSAKxML1E2NmlmSFRDdz0g";
+ DeviceAddress waterSensor = {0x28, 0x1E, 0x55, 0x14, 0x04, 0x00, 0x00, 0xB9};
+ DeviceAddress ambientSensor = {0x28, 0xBB, 0x55, 0x14, 0x04, 0x00, 0x00, 0x0E};
+ */
 
 // Used to save the last point in time that a report was sent
 unsigned long lastReportTime;
@@ -32,105 +45,30 @@ unsigned long lastReportTime;
 // Used to save the last point in time we took a temperature reading
 unsigned long lastTempSampleTime;
 
+String msg;
+
 // Create a soft serial port on pins 7(RX) and 8(TX) for modem
 SoftwareSerial modem(7,8);
 
-char incoming_char = 0;
-char strBuffer[288];
-
-unsigned char buffer[64]; // buffer array for data recieve over serial port
-int count=0;     // counter for buffer array 
-
-void modemSend(String command, int interTime=1000){
-  modem.print(command);
-  modem.write(byte(13));
-  delay(interTime);
-}
-
-
-PString outputJSON(void)
+ISR(WDT_vect)
 {
-  PString jsonOut(strBuffer,sizeof(strBuffer));
-  jsonOut.print("{\"version\":\"1.0.0\",\"datastreams\":");
-  jsonOut.print("[{\"id\":\"Water\", \"current_value\":\"");
-  jsonOut.print(waterTemp);
-  jsonOut.print("\"},");
-  jsonOut.print("{\"id\":\"Air\", \"current_value\":\"");
-  jsonOut.print(ambientTemp);
-  jsonOut.print("\"}]}");
-  return(jsonOut);
-}
-
-PString outputCSV(void)
-{
-  PString csvOut(strBuffer,sizeof(strBuffer));
-  csvOut.print("Water,");
-  csvOut.println(waterTemp);
-  csvOut.print("Air,");
-  csvOut.println(ambientTemp);
-  return(csvOut);
-}
-
-void powerUpOrDown(void)
-{
-  pinMode(9, OUTPUT); 
-  digitalWrite(9,LOW);
-  delay(1000);
-  digitalWrite(9,HIGH);
-  delay(2000);
-  digitalWrite(9,LOW);
-  delay(10000);
-}
-
-void establishNetwork(void)
-{
-  modemSend("AT");
-  modemSend("AT+SAPBR=3,1,\"Contype\",\"GPRS\"",2000);
-  modemSend("AT+SAPBR=3,1,\"APN\",\""+String(APN)+"\"",5000);
-  modemSend("AT+SAPBR=1,1",5000);
-  modemSend("AT+SAPBR=2,1",2000);
-}
-
-void shutdownNetwork(void)
-{
-  Serial.println("Shutting down GPRS context");
-  modemSend("AT+SAPBR=0,1",2000);
-}
-
-void sleepModem(boolean yesNo)
-{
-  if( yesNo == true ) {
-    modemSend("AT+CSCLK=2");
-  } else {
-    modemSend("AT+CSCLD=0");
+  if(f_wdt == 0)
+  {
+    f_wdt=1;
+  }
+  else
+  {
+    Serial.println("WDT Overrun!!!");
   }
 }
 
-void sendToCosm(void)
-{
-  PString json=(strBuffer,sizeof(strBuffer),outputCSV());
-  int jsonSize=json.length();
-  String url;
-  url = "http://api.cosm.com/v2/feeds/"+String(SENSOR_ID)+".csv?_method=put&key="+String(COSM_API_KEY);
-  Serial.println("Sending data via HTTP");
-  //Serial.println(json);
-  modemSend("AT+HTTPINIT");
-  modemSend("AT+HTTPPARA=\"CID\",1");
-  modemSend("AT+HTTPPARA=\"URL\",\""+url+"\"");
-  modemSend("AT+HTTPDATA="+String(jsonSize)+",3000");
-  modemSend(String(json));
-  modemSend("AT+HTTPACTION=1");
-  modemSend("AT+HTTPTERM");
-}
-  
-void setup(void)
+void setup()
 {
   // Start the serial ports
-  Serial.begin(9600);  
-  modem.begin(9600);
- 
-  Serial.println("I'm Alive");
-  modemSend("ATE1");
+  Serial.begin(19200);  
+  modem.begin(19200);
+  delay(500);
+
   // Start up the temperatue sensors
   sensors.begin();
 
@@ -138,54 +76,176 @@ void setup(void)
   sensors.setResolution(waterSensor, TEMPERATURE_PRECISION);
   sensors.setResolution(ambientSensor, TEMPERATURE_PRECISION);
 
+  // Reset the counter
   lastReportTime = lastTempSampleTime = millis();
+
+  /*** Setup the WDT ***/
+
+  /* Clear the reset flag. */
+  MCUSR &= ~(1<<WDRF);
+
+  /* In order to change WDE or the prescaler, we need to
+   * set WDCE (This will allow updates for 4 clock cycles).
+   */
+  WDTCSR |= (1<<WDCE) | (1<<WDE);
+
+  /* set new watchdog timeout prescaler value */
+  WDTCSR = 1<<WDP0 | 1<<WDP3; /* 8.0 seconds */
+
+  /* Enable the WD interrupt (note no reset). */
+  WDTCSR |= _BV(WDIE);
 }
 
-void sampleTemps(void)
+void loop()
 {
-  // Get the temperatures from both sensors
+  if(f_wdt == 1)
+  {
+    delay(1000);
+    unsigned long newReportTime = millis();
+
+    // Or send a message if we have reached the report interval
+    if ((newReportTime - lastReportTime) >= REPORT_INTERVAL)
+    {
+      wdt_disable();
+      lastReportTime = newReportTime;
+      powerUp();
+      sendToCosm();
+      powerDown();
+      wdt_enable(8000);
+    }
+    if (modem.available())
+      Serial.write(modem.read());
+
+    f_wdt = 0;
+    enterSleep();   
+  }
+}
+
+void enterSleep(void)
+{
+  set_sleep_mode(SLEEP_MODE_PWR_SAVE);   /* EDIT: could also use SLEEP_MODE_PWR_DOWN for lowest power consumption. */
+  sleep_enable();
+
+  /* Now enter sleep mode. */
+  sleep_mode();
+
+  /* The program will continue from here after the WDT timeout*/
+  sleep_disable(); /* First thing to do is disable sleep. */
+
+  /* Re-enable the peripherals. */
+  power_all_enable();
+}
+
+void ShowSerialData()
+{
+  while(modem.available()!=0)
+    Serial.write(modem.read());
+}
+
+void powerUp()
+{
+  pinMode(9, OUTPUT); 
+  digitalWrite(9,LOW);
+  delay(1000);
+  digitalWrite(9,HIGH);
+  delay(2000);
+  digitalWrite(9,LOW);
+  delay(3000);
+  delay(20000);
+}
+
+void powerDown()
+{
+  modem.println("AT+CPOWD=1");
+}
+void sendToCosm()
+{ 
   sensors.requestTemperatures();
   waterTemp = sensors.getTempC(waterSensor);
   ambientTemp = sensors.getTempC(ambientSensor);
+
+  modem.println("AT+CGATT?");
+  delay(100);
+
+  ShowSerialData();
+
+  modem.println("AT+SAPBR=3,1,\"Contype\",\"GPRS\"");
+  delay(500);
+
+  ShowSerialData();
+
+  modem.println("AT+SAPBR=3,1,\"APN\",\""+APN+"\"");
+  delay(500);
+
+  ShowSerialData();
+
+  modem.println("AT+SAPBR=1,1");
+  delay(5000);
+
+  ShowSerialData();
+
+  modem.println("AT+SAPBR=2,1");
+  delay(5000);
+
+  ShowSerialData();
+
+  modem.println("AT+HTTPINIT");
+  delay(200);
+
+  ShowSerialData();
+
+  modem.println("AT+HTTPPARA=\"CID\",1");
+  delay(200);
+
+  ShowSerialData();
+
+  modem.print("AT+HTTPPARA=\"URL\",");
+  delay(200);
+
+  ShowSerialData();
+
+  modem.print("\"http://api.cosm.com/v2/feeds/"+SENSOR_ID+".csv?");
+  delay(200);
+
+  ShowSerialData();
+
+  modem.println("_method=put&key="+COSM_API_KEY+"\"");
+  delay(500);
+
+  ShowSerialData();
+
+  modem.println("AT+HTTPDATA=22,2000");
+  delay(100);
+
+  ShowSerialData();
+
+  modem.print("Water,");
+  modem.println(waterTemp);
+  modem.print("Air,");
+  modem.println(ambientTemp);
+  delay(2000);
+
+  modem.println("AT");
+  delay(200);
+
+  ShowSerialData();
+
+  modem.println("AT+HTTPACTION=1");
+  delay(5000);
+
+  ShowSerialData();
+
+  modem.println("AT+HTTPTERM");
+  delay(5000);
+
+  ShowSerialData();
+
+  modem.println("AT+SAPBR=0,1");
+  delay(5000);
+
+  ShowSerialData();
 }
 
-void loop(void)
-{
-  //LowPower.idle(SLEEP_1S,ADC_OFF,TIMER2_OFF,TIMER1_OFF,TIMER0_ON,SPI_OFF,USART0_OFF,TWI_OFF);  
-  //LowPower.powerDown(SLEEP_1S,ADC_OFF,BOD_OFF);  
 
-  unsigned long newReportTime = millis();
-  
-  // Or send a message if we have reached the report interval
-  if ((newReportTime - lastReportTime) > REPORT_INTERVAL)
-  {
-    Serial.println("Sending Report");
-    sampleTemps();
-    lastReportTime = newReportTime;
-    //powerUpOrDown();
-    establishNetwork();
-    sendToCosm();
-    shutdownNetwork();
-    //powerUpOrDown();
-  }
-  
-  while (modem.available() > 0)
-  {
-    // Take any incoming data from the SM5100B modem and send it to the terminal
-    {
-      buffer[count++]=modem.read();     // writing data into array
-      if(count == 64)break;
-    }
-    Serial.write(buffer,count);      // if no data transmission ends, write buffer to hardware serial port
-    clearBufferArray();              // call clearBufferArray function to clear the storaged data from the array
-    count = 0;                       // set counter of while loop to zero
-  }
-  
-  // Relay input from the terminal to the SM5100B modem
-  while (Serial.available() > 0)
-  {
-    // If the character is CTRL-Z then set a flag we will check later to print the current temps to the terminal
-     modem.write(Serial.read());
-  }
-  
-}
+
+
